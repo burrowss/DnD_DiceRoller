124cdf2574b66fe5fa2d75baada92255444a6aa9
burrowss <burrowss@allegheny.edu>
2018-10-29 19:39:04 -0400

    initial code commit. Includes jumping cube in a box

---
+++ DnD_DiceRoller/Assets/RandomDirectionalForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDirectionalForce : MonoBehaviour {
+
+	public string button = "Fire1";
+	public float newtons = 10.0f;
+	public float torque = 10.0f;
+	public ForceMode forceMode;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(button)){
+			GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * newtons, forceMode);
+			GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere * torque, forceMode);
+		}
+	}
+}
